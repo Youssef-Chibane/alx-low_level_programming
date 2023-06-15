9c79f7807a837266c2af653bda7e2d3a0bd90275
Youssef-Chibane <chib4ne.youssef@gmail.com>
2023-06-15 15:24:52 -0700

    4 task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - entry function
+* Description: printing a message
+* Return: 0 if exited properly
+*/
+
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+}
