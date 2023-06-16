835687f7d794e765427090ccb895ada3abbfecdc
Youssef-Chibane <chib4ne.youssef@gmail.com>
2023-06-16 16:53:50 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - entry function
+* Description: print alphabet in lowercase and upperrcase
+* Return: 0 if exited properly
+*/
+
+int main(void)
+{
+         char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; ++c)
+	{
+		putchar(c);
+	}
+		putchar('\n');
+
+         return (0);
+}
