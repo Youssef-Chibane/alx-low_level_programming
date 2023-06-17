48cb1dab9b4b5eb5234a4e375ff57c09d98fceac
Youssef-Chibane <chib4ne.youssef@gmail.com>
2023-06-16 18:14:53 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - entry function
+* Description: print all possible combinations of single-digit numbers
+* Return: 0 if exited properly
+*/
+
+int main(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+	{
+		putchar(n);
+		if (x != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
