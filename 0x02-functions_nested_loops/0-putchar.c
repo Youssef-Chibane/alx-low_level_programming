ac9c2dba126da7f7ce3420ec507265558fac7881
Youssef-Chibane <chib4ne.youssef@gmail.com>
2023-06-20 08:43:53 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - entry function
+ * Description: program that print "_putchar"
+ * Return: 0 if program exited successfully
+*/
+
+int main(void)
+{
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+}
