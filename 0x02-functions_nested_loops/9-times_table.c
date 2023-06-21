fc30bede32a968bc4d246c4753cc75d5aa103812
Youssef-Chibane <chib4ne.youssef@gmail.com>
2023-06-20 18:48:45 -0700

    last try right!

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,12 +18,12 @@ void times_table(void)
 				_putchar(r + '0');
 			}
 
-			if (k < 10 && j != 0)
+			if (r < 10 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(k + '0');
+				_putchar(r + '0');
 			} else if (r >= 10)
 			{
 				_putchar(',');
