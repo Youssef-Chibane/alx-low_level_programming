d67e356b5a0b379bf7ef4278708072001788fc48
Youssef-Chibane <chib4ne.youssef@gmail.com>
2023-06-22 13:23:24 -0700

    task 04 betty

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
-/** print_most_numbers - function that prints the numbers,
-from 0 to 9, followed by a new line.
+/**
+* print_most_numbers - function that prints the numbers,
+* from 0 to 9, followed by a new line.
 * Do not print 2 and 4
 */
 
