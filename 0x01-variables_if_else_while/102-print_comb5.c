a050ceafb74c1ce898d6f3ace5816414f1d77cf0
Youssef-Chibane <chib4ne.youssef@gmail.com>
2023-06-24 17:48:07 -0700

    betty fix

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,12 +10,13 @@
 int main(void)
 {
 	int num1 = 0, num2;
+
 	while (num1 <= 99)
 	{
 		num2 = num1;
 		while (num2 <= 99)
 		{
-			if( num1 != num2)
+			if (num1 != num2)
 			{
 				putchar((num1 / 10) + 48);
 				putchar((num1 % 10) + 48);
