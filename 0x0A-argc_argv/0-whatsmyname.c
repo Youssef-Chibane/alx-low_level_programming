8dd71315e7b6d5a0512c09a92ea736120902a612
Youssef-Chibane <chib4ne.youssef@gmail.com>
2023-07-07 15:44:10 -0700

    task 00 betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
-*
-*
-*
+* main - program that prints its name, followed by a new line.
+* @argc: number of arguments.
+* @argv: array of arguments.
+* Return: return 0 if the program implemented successfully.
 */
 
 int main(int argc, char *argv[])
