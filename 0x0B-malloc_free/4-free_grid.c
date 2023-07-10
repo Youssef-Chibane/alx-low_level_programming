8f418087685b5dabec0ad3710199a92e1dd54d82
Youssef-Chibane <chib4ne.youssef@gmail.com>
2023-07-10 16:58:31 -0700

    betty doc

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,10 +2,9 @@
 #include <stdlib.h>
 
 /**
-*
-*
-*
-*
+* free_grid  - frees a 2 dimensional grid previously created.
+* @grid: the grid to free.
+* @height: height of the grid.
 */
 
 void free_grid(int **grid, int height)
