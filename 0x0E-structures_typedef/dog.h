485c44550e20aedd130afc87b48bb6796bfd2865
Youssef-Chibane <chib4ne.youssef@gmail.com>
2023-07-17 16:45:12 -0700

    task 00

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+* struct dog - information about dog.
+* @name: name of the dog.
+* @age: age of the dog.
+* @owner: owner of the dog.
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
